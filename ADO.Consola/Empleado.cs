b3902859c0f968be61e3e530e26d322eaa9090bc
draffe <draffe@mvfactory.com>
2023-12-12 20:05:39 -0300

    .

---
+++ ADO.Consola/Empleado.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace ADO.Consola
+{
+    public class Empleado
+    {
+        public int Id { get; set; }
+        public string Nombre { get; internal set; }
+        public string Apellido { get; internal set; }
+
+        public Empleado(int id, string nombre, string apellido)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+        public override string ToString()
+        {
+            return Nombre + " " + Apellido;
+        }
+    }
+}
---
+++ ADO.Consola/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ADO.Conectar();
+                Console.WriteLine("Conexion exitosa");
+
+                Console.WriteLine(ADO.ContarProductos());
+
+                ADO.GuardarEmpleado(new Empleado(0, "Dixon", "Raffe"));
+                ADO.GuardarEmpleado(new Empleado(0, "Dixon 2 ", "Raffe"));
+                ADO.GuardarEmpleado(new Empleado(0, "Dixon 23", "Raffe"));
+                ADO.GuardarEmpleado(new Empleado(0, "Dixon 123", "Raffe"));
+
+                ADO.GuardarEmpleado(new Empleado(0, "Dixon asdas", "Raffe"));
+                ADO.GuardarEmpleado(new Empleado(0, "Dixon asdas" , "Raffe"));
+
+                var listaempleados = ADO.ListarEmpleados();
+
+                foreach (var item in listaempleados)
+                { 
+                  Console.WriteLine(item.ToString());
+                }
+
+                var listadeproductos = ADO.TopTenProductosCaros();
+
+                foreach (var item in listadeproductos)
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.ToString());
+            }
+            
+           Console.ReadLine();
+        }
+    }
+}
